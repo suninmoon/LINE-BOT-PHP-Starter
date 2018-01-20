cbf29ce50ad7907c9f5d43ebd7c99e21472657b5
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-01-21 05:36:05 +0700

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,16 @@
+<?php
+$access_token = 'kLXmlDjldD7wdVPbTRSASV2fuZ/s5H/S6TQRVMbUTSpH/TN76VAykqALraGPOxQuKZBA3+az4bnx/HXR3lKy+B1GeBad168RXoCQXqgD4W0SlMMHOzvJWyVFlhDq1qexH9Hs8uCD4nrDAsi6e5GEegdB04t89/1O/w1cDnyilFU=
+';
+
+$url = 'https://mailline.herokuapp.com/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
