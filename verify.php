80370ebb509b76ba385d68069fb9041990562298
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-01-21 12:45:26 +0700

    verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,12 @@
 <?php
 $access_token = 'Ke4AD7dO7T4uy2KuNUueCSpwn4ja4UbM8oFPXz9ybpFjy9j7igeFF1l0V1f1p7jUEZTO3xGU+rsRJUiZOZ/GYJKB/NGFPQxLxXZ+rGeD8LFzNuGGLY7927r3vKg0jfFLmr5Hqtu0XQs3F7jPeMJjIAdB04t89/1O/w1cDnyilFU=';
 
+$proxy = 'proxyurl:port';
+$proxyauth = 'username:password';
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
+
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
