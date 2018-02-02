8e42698c90cb3e0ad0fe756cbe4e89673cceb46d
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-02 10:34:28 +0700

    bot.php

--- bot.php
+++ bot.php
@@ -13,14 +13,14 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['1'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => [$content],
+				'type' => '1',
+				'1' => [$content],
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
