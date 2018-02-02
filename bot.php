6f569d2d15884df35e93a16c47f4dc24a4aedd24
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-02 13:33:15 +0700

    bot.php

--- bot.php
+++ bot.php
@@ -5,27 +5,27 @@ $proxy = 'http://fixie:MW30JqTyxH5cpYt@velodrome.usefixie.com:80';
 $proxyauth = 'pondmyinlove@gmail.com:0836458085Pp';
 
 $events = json_decode($content, true);
-$events0 = json_decode($content0, true);
-$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
-$content0 = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
+//$events0 = json_decode($content0, true);
+//$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
+//$content0 = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == '$messages' && $event['$messages']['type'] == '1') {
 			//$events = json_decode($content, true);
-		//$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
+	$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
 
 //$content = file_get_contents('php://input');
 			// Get text sent
-			$Text = $event['message']['text'];
+			$Text = $event['message']['1'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 		
 			// Build message to reply back
 			$messages = [
 				'type' == '1',
-				'text' == '$content',
+				'1' == '$content',
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -51,9 +51,13 @@ curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 			echo $result . "\r\n";
-		}
+				}
+		echo "ok";
+	}
 
-		if ($event0['type0'] == '$messages0' && $event0['$messages0']['type0'] == '0') {
+}
+
+		/*if ($event0['type0'] == '$messages0' && $event0['$messages0']['type0'] == '0') {
 			//$content = file_get_contents('php://input');
 			//$events0 = json_decode($content0, true);
 //$content0 = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
@@ -95,4 +99,4 @@ curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 		echo "ok";
 	}
 
-}
+}*/
