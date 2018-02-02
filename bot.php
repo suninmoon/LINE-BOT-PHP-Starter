27fbad92023422fecae5c2d31b7ae2282d18fa78
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-02 21:55:46 +0700

    bot.php

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => '1',
-				'text' => [$content],
+				'1' => [$content],
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
