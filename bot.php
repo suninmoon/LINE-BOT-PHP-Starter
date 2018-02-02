c869fddae3efa3aaebbdf628fb52b5542ba67a7f
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-02 10:50:27 +0700

    bot.php

--- bot.php
+++ bot.php
@@ -7,6 +7,7 @@ $proxyauth = 'pondmyinlove@gmail.com:0836458085Pp';
 //$content = file_get_contents('php://input');
 $events = json_decode($content, true);
 $content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
+$contentt = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
@@ -48,6 +49,52 @@ curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			echo $result . "\r\n";
 		}
 		
-	}
+	}echo "http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1";
 }
-echo "http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1";
+
+
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == '$messages' && $event['$messages']['type'] == 'Text') {
+			// Get text sent
+			$Text = $event['message']['Text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' == '0',
+				'Text' == [$contentt],
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			
+			$data = [
+				
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+		
+	}
+echo "http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0";
+}
+
