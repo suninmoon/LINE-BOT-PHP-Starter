c93869e0f2034b217cd261cb3b883e316fea8ae9
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-02 21:39:55 +0700

    bot.php

--- bot.php
+++ bot.php
@@ -4,49 +4,49 @@ $access_token = 'Ke4AD7dO7T4uy2KuNUueCSpwn4ja4UbM8oFPXz9ybpFjy9j7igeFF1l0V1f1p7j
 $proxy = 'http://fixie:MW30JqTyxH5cpYt@velodrome.usefixie.com:80';
 $proxyauth = 'pondmyinlove@gmail.com:0836458085Pp';
 
-$eventsA = json_decodeA($contentA, true);
-$contentA = file_get_contentsA('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
+$events = json_decode($contentA, true);
+$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
 
-$eventsB = json_decodeB($contentB, true);
-$contentB = file_get_contentsB('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
+//$eventsB = json_decodeB($contentB, true);
+//$contentB = file_get_contentsB('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
 
-if (!is_null($eventsA['eventsA'])) {
+if (!is_null($events['events'])) {
 
-	foreach ($eventsA['eventsA'] as $eventA) {
+	foreach ($events['events'] as $event) {
 
-		if ($eventA['typeA'] == '$messagesA' && $eventA['$messagesA']['typeA'] == '1') {
+		if ($event['type'] == '$messages' && $event['$messages']['type'] == '1') {
 
-			$TextA = $eventA['messageA']['textA'];
+			$Text = $event['message']['1'];
 
-			$replyTokenA = $eventA['replyTokenA'];
+			$replyToken = $event['replyToken'];
 
-			$messagesA= [
-				'typeA' == '1',
-				'textA' == '$contentA',
+			$messages= [
+				'type' == '1',
+				'1' == '$content',
 			];
 
 			
-			$urlA = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			
-			$dataA = [
+			$data = [
 				
-				'replyTokenA' => $replyTokenA,
-				'messagesA' => [$messagesA],
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
 			];
-			$postA = json_encodeA($data);
-			$headersA = array('ContentA-TypeA: application/json', 'Authorization: Bearer ' . $access_token);
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$chA = curl_init($urlA);
-			curl_setopt($chA, CURLOPT_PROXY, $proxy);
-curl_setopt($chA, CURLOPT_PROXYUSERPWD, $proxyauth);
-			curl_setopt($chA CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($chA, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($chA, CURLOPT_POSTFIELDS, $postA);
-			curl_setopt($chA, CURLOPT_HTTPHEADER, $headersA);
-			curl_setopt($chA, CURLOPT_FOLLOWLOCATION, 1);
-			$resultA = curl_exec($chA);
-			curl_close($chA);
-			echo $resultA . "\r\n";
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			curl_setopt($ch CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$resultA = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 		}
 		
 	}
