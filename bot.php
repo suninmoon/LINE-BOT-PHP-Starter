3c3a0e99b1b423fe73408d2657e2ef259035f757
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-02 11:03:10 +0700

    bot.php

--- bot.php
+++ bot.php
@@ -4,15 +4,14 @@ $access_token = 'Ke4AD7dO7T4uy2KuNUueCSpwn4ja4UbM8oFPXz9ybpFjy9j7igeFF1l0V1f1p7j
 $proxy = 'http://fixie:MW30JqTyxH5cpYt@velodrome.usefixie.com:80';
 $proxyauth = 'pondmyinlove@gmail.com:0836458085Pp';
 
-//$content = file_get_contents('php://input');
-$events = json_decode($content, true);
-$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
-//$contentt = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == '$messages' && $event['$messages']['type'] == '1') {
+			//$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1');
 			// Get text sent
 			$Text = $event['message']['Text'];
 			// Get replyToken
@@ -49,7 +48,47 @@ curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			echo $result . "\r\n";
 		}
 		
-	}echo "http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1";
+		if ($event['type'] == '$messages' && $event['$messages']['type'] == '0') {
+			//$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+$content = file_get_contents('http://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/0');
+			// Get text sent
+			$Text = $event['message']['Text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+		
+			// Build message to reply back
+			$messages = [
+				'type' == '0',
+				'Text' == [$content],
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			
+			$data = [
+				
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+		
+	}
 }
 
-
+echo "OK";
