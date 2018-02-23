d0457855727910b5539e4ff017d5bf58b4e6820a
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-24 02:58:07 +0700

    index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-<head>
-  <link rel="stylesheet" href="styles.css">
-</head>
-
 <form>
-<input style="width: 300px; padding: 20px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+<input style="width: 900px; padding: 80px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
 </form>
  
 <?php
