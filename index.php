dfb3fc457a9a23964cb00dcfbee4420babe711fb
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-23 23:42:12 +0700

    index.html

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a8f26de29d137bb"></script>
-echo "Hello LINE BOT";
