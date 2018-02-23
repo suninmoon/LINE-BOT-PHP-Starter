ea5fc76211c5035c4524a188309707f49210098b
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-24 03:23:34 +0700

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <form>
-<input style="width: 900px; padding: 500px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" onclick=“window.close().href='http://www.hyperlinkcode.com/button-links.php'” />
+<input style="width: 900px; padding: 500px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" ONCLICK="window.location.href='https://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1'" />
 </form>
  
 <?php
