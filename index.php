ace991a6d86bf5b698a82f21ddba4f5e3c9a5a24
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-24 02:21:40 +0700

    index.php

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
-nes (2 sloc)  29 Bytes
-
 <FORM>
-<INPUT TYPE="BUTTON" VALUE="Home Page" ONCLICK="window.location.href='http://www.tip-com.net'">
+<INPUT TYPE="BUTTON" VALUE="Home Page" ONCLICK="window.location.href='https://www.facebook.com/'">
 </FORM>
  
-<script type="text/javascript">
-function popupModal(url,width,height){
-    var myDate=new Date();
-    var setUniqe=myDate.getTime(); // ใช้สำหรับ ป้องกันการ cache กรณีกำลังทดสอบ
-     
-    var diaxFeature="dialogWidth:"+width+"px;"
-    +"dialogHeight:"+height+"px;"
-/*  +"dialogLeft:"+width+"px;"
-    +"dialogTop:"+width+"px;"       */
-    +"center:yes;"
-    +"edge:raised;" // sunken | raised 
-    +"resizable:no;"
-    +"status:no;"
-    +"scroll:no;";  
-    window.showModalDialog(url+"?"+setUniqe,"", diaxFeature);
-    // กรณีไม่ใช้ setUniqe
-    //  window.showModalDialog(url,"", diaxFeature); 
-}
-</script>
-
-
 <?php
 echo "Hello LINE BOT";
