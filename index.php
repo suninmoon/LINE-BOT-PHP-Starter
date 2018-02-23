72411dcdfbbf9a8701d0a8f480358ea4e84bbfdd
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-24 02:38:27 +0700

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<FORM>
-<INPUT TYPE="BUTTON" VALUE="Home Page" ONCLICK="window.location.href='https://www.facebook.com/'">
-</FORM>
+<form>
+<input style="width: 300px; padding: 20px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+</form>
  
 <?php
 echo "Hello LINE BOT";
