b5e4d2616106eba66fe3dd2a8eb88f47dd99586d
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-24 13:22:34 +0700

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <form>
-<input style="width: 900px; height: 900 px;padding: 500px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" ONCLICK="window.location.href='https://l.facebook.com/l.php?u=http%3A%2F%2Fline.me%2Fti%2Fg%2FP3KaOMWi_l&h=ATOWvC2GhKEb-epHB-3B4azsOpmDMgCel3ctdpH2lXcjo0Tw2dRrHmXdvNTSA5izTvP1ZeQ_FfZIxFgb4B3RdLeldCFpSUAKZdtW3wuQ_9vE4SvjkkLEHA'" />
-<button onClick = “window.close()”> ข้อความ </button>
+<input style="width: 900px; height: 900 px;padding: 500px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" ONCLICK="window.location.href='https://line.me/R/ti/p/%40iej4754u'" />
+ <button onClick = “window.location.href='https://line.me/R/ti/p/%40iej4754u' window.close()”> ข้อความ </button>
 </form>
  
 <?php
