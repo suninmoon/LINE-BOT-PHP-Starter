6deb2ec7f729092c3e469ff3942ac2591690db07
suninmoon <35640769+suninmoon@users.noreply.github.com>
2018-02-24 13:13:48 +0700

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <form>
-<input style="width: 900px; height: 900 px;padding: 500px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" ONCLICK="window.location.href='https://api.anto.io/channel/set/tZlbFUDeFGiTmSxQEjm3PaOdtQM7g024mJOuYp64/Messaging/button1/1'" />
+<input style="width: 900px; height: 900 px;padding: 500px; cursor: pointer; box-shadow: 6px 6px 5px; #999; -webkit-box-shadow: 6px 6px 5px #999; -moz-box-shadow: 6px 6px 5px #999; font-weight: bold; background: #ffff00; color: #000; border-radius: 10px; border: 1px solid #999; font-size: 150%;" type="button" value="Put Your Text Here" ONCLICK="window.location.href='https://l.facebook.com/l.php?u=http%3A%2F%2Fline.me%2Fti%2Fg%2FP3KaOMWi_l&h=ATOWvC2GhKEb-epHB-3B4azsOpmDMgCel3ctdpH2lXcjo0Tw2dRrHmXdvNTSA5izTvP1ZeQ_FfZIxFgb4B3RdLeldCFpSUAKZdtW3wuQ_9vE4SvjkkLEHA'" />
 <button onClick = “window.close()”> ข้อความ </button>
 </form>
  
